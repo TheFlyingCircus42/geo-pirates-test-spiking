b04b0e8a110c0ac4ce35c01a99cf8bb2632ff632
Zsolt <zsolt@wavedropper.com>
2026-03-04 16:24:30 +0000

    Install expo-location

--- geo-pirates-test/app/index.jsx
+++ geo-pirates-test/app/index.jsx
@@ -1,7 +1,36 @@
+import { useState, useEffect } from "react";
 import { Text, View } from "react-native";
-import { Link } from "expo-router";
+import { Link, useRouter } from "expo-router";
+import { Button } from "react-native";
+import * as Location from "expo-location";
 
 export default function Index() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const router = useRouter();
+
+  useEffect(() => {
+    async function getCurrentLocation() {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    }
+
+    getCurrentLocation();
+  }, []);
+
+  let text = "Waiting...";
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
   return (
     <View
       style={{
@@ -9,8 +38,11 @@ export default function Index() {
         justifyContent: "center",
         alignItems: "center",
       }}
+      className="flex flex-col w-full h-full gap-3"
     >
-      <Text className="text-red-500">Hello</Text>
+      <Text className="text-red-500 text-5xl">Hello</Text>
+      <Text className="text-blue-600 font-bold">Your location is</Text>
+      <Text className="break-all max-w-60"> {text}</Text>
 
       <Link href="/newPage">
         <Text>New Page Link</Text>
